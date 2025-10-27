fc570b251af328870bb9e8ca89fff4f3002646e6
Sonali301 <sonalikumari7817@gmail.com>
2025-10-28 00:21:08 +0530

    made changes in classroom folder for the routes

--- CLASSROOM/routes/post.js
+++ CLASSROOM/routes/post.js
@@ -1,25 +1,25 @@
-// const express= require("express");
-// const router = express.Router(); // router object
+const express= require("express");
+const router = express.Router(); // router object
 
-// // POST
-// // INDEX - POST
-// router.get("/",(req,res)=>{
-//     res.send("GET for posts");
-// })
+// POST
+// INDEX - POST
+router.get("/",(req,res)=>{
+    res.send("GET for posts");
+})
 
-// // SHOW -POST
-// router.get("/:id",(req,res)=>{
-//     res.send("GET for show post id");
-// })
+// SHOW -POST
+router.get("/:id",(req,res)=>{
+    res.send("GET for show post id");
+})
 
-// // POST-POST
-// router.post("/",(req,res)=>{
-//     res.send("Post for posts");
-// })
+// POST-POST
+router.post("/",(req,res)=>{
+    res.send("Post for posts");
+})
 
-// // DELETE-POST
-// router.delete("/:id",(req,res)=>{
-//     res.send("delete for post id");
-// })
+// DELETE-POST
+router.delete("/:id",(req,res)=>{
+    res.send("delete for post id");
+})
 
-// module.exports = router;
+module.exports = router;
